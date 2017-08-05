4c80757d9c71003f64bf91ecb8e920bd124e6dcb
Jimeno0 <c.p.jimeno0@gmail.com>
2017-08-06 00:07:24 +0200

    feat(Master commit 💥): Working with es7

---
+++ src/index.js
@@ -0,0 +1,15 @@
+console.log('Compiled and added botchieeeeeeees');
+
+const obj = {
+  a: 'a',
+  b: 'b',
+};
+
+const es6obj = { ...obj };
+
+console.log(es6obj);
+
+// const arr = [1, 34];
+// const arres6 = [...arr];
+//
+// console.log(arres6);
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'build.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+};
+
+module.exports = config;
