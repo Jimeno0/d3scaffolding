dddf113c316dada543dbb39a66a584bbdad4cb1d
Jimeno0 <c.p.jimeno0@gmail.com>
2017-08-06 00:58:35 +0200

    feat(Basic extent): Extent for array and objects arrays

--- src/index.js
+++ src/index.js
@@ -2,8 +2,22 @@ import * as d3 from 'd3';
 
 const data = [1, 2, 3, 4];
 
-const min = d3.min(data);
-const max = d3.max(data);
+// const min = d3.min(data);
+// const max = d3.max(data);
+const minMax = d3.extent(data);
+console.log(minMax);
 
-console.log(min, max);
+// GET FORM OBJ
 
+const dataObj = [
+  { key: 'Jimeno0', value: 28 },
+  { key: 'Vilva', value: 26 },
+  { key: 'Paco', value: 25 },
+  { key: 'Grana', value: 29 },
+  { key: 'Alfon', value: 32 },
+];
+
+const maxObj = d3.max(dataObj, elem => elem.value);
+const extentObj = d3.extent(dataObj, elem => elem.value);
+console.log('max from obj', maxObj);
+console.log('extent from object', extentObj);
