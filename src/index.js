9fbc620c5b26d37713053b213bf0e10d438e1132
Jimeno0 <c.p.jimeno0@gmail.com>
2017-08-12 20:55:23 +0200

    feat(Scale minimun barchart to fill the svg):

--- src/index.js
+++ src/index.js
@@ -9,13 +9,21 @@ const data = [
   { key: 'Alfon', value: 32 },
 ];
 
-// const maxObj = d3.max(data, elem => elem.value);
-// const extentObj = d3.extent(data, elem => elem.value);
+const maxObj = d3.max(data, elem => elem.value);
+const extentObj = d3.extent(data, elem => elem.value);
 
 
 // Create programatically our chart
 const h = 600;
 const w = 800;
+
+// Scalling data to fullfit svg
+const x = d3.scaleLinear()
+  .domain([0, maxObj])
+  .range([0, w]);
+const y = d3.scaleLinear()
+  .domain([0, data.length])
+  .range([0, h]);
 
 const svg = d3.select('body').append('svg')
             .attr('id', 'chart')
@@ -28,6 +36,6 @@ svg.selectAll('.bar')
   .append('rect')
   .attr('class', 'bar')
   .attr('x', 0)
-  .attr('y', (data, i) => i * 20)
-  .attr('height', 19)
-  .attr('width', (data, i) => data.value);
+  .attr('y', (data, i) => y(i))
+  .attr('height', (data, i) => y(1) - 1) // take the fisrt height and remove 1px
+  .attr('width', (data, i) => x(data.value));
